63de129c53b2e41b30784dec906bc1de8bbb9208
Alex Sage <sagezander@gmail.com>
2025-02-20 16:35:21 -0600

    fix: Add create organization to route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const app = require('./server-config.js');
 const routes = require('./server-routes.js');
+const createOrganization = require('./routes/organization/createOrganization.js');
 
 const port = process.env.PORT || 5000;
 
 app.get('/api/v1/organization/:id');
 app.patch('/api/v1/organization/:id');
-app.post('/api/v1/organization');
+app.post('/api/v1/organization', createOrganization);
 app.delete('/api/v1/organization/:id');
 
 
